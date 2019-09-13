b205c92171ab368c2931068d34bc04ad7f4dd641
zhangbx <496691544@qq.com>
2019-09-13 19:48:03 +0800

    增加配置

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import Test from './modules/Test'
+import './App.css'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code> src / App.js </code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,9 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <Test />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
