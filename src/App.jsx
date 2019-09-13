350c70bdb36e799e87a3ec2abd170807e42011dc
zhangbx <496691544@qq.com>
2019-09-13 20:37:06 +0800

    增加配置

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+
         <Test />
       </header>
     </div>
